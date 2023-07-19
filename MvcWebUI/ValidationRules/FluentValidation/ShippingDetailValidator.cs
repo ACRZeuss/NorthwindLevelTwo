818e1d55cf74dfe9b0abbb070f8564d1a2af8a7d
Erhan <ep4963807@gmail.com>
2023-07-19 14:30:39 +0300

    Add project files.

---
+++ MvcWebUI/ValidationRules/FluentValidation/ShippingDetailValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MvcWebUI.Models;
+
+namespace MvcWebUI.ValidationRules.FluentValidation
+{
+    public class ShippingDetailValidator : AbstractValidator<ShippingDetail>
+    {
+        public ShippingDetailValidator()
+        {
+            RuleFor(s => s.FirstName).NotEmpty();
+            RuleFor(s => s.FirstName).MinimumLength(3);
+            RuleFor(s => s.LastName).NotEmpty();
+            RuleFor(s => s.Address).NotEmpty();
+
+            RuleFor(s => s.City).NotEmpty().When(s => s.Age < 18);
+        }
+    }
+}
